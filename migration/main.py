068d60dc886efce6b44fda435ecd6c2f25e9e693
obabawale <padinality@yahoo.com>
2024-08-05 20:26:39 +0100

    compress functions into one

--- migration/main.py
+++ migration/main.py
@@ -1,36 +1,51 @@
 from decouple import config
 from connector import connector
 
+# Generic model for the update
 
-def update_login(models, db, uid, password) -> None:
-    users = models.execute_kw(db, uid, password, 'res.users', 'search_read', [], {'fields': ['name', 'login']})
-    for user in users:
-        # Before update
-        print("Before...", user['login'])
-        if not 'asseco.ng' in user['login']:
-            continue
 
-        # update user login
-        new_login = user['login'].replace('asseco.ng', 'axendit.com')
+def update_models(models, db, uid, password) -> None:
+    models_to_update = [
+        ('res.users', 'email'),
+        ('res.partner', 'email'),
+        ('hr.employee', 'work_email'),
+    ]
 
-        # update user login
-        # models.execute_kw(db, uid, password, 'res.users', 'write', [
-                        #   user['id'], {'login': new_login}])
-        # after update
-        [new_user] = models.execute_kw(db, uid, password, 'res.users', 'search_read', [
-            [['id', '=', user['id']]]], {'fields': ['name', 'login']})
-        print(f"After..., {new_user['login']}")
+    for Model, field in models_to_update:
+        records = models.execute_kw(db, uid, password, Model, 'search_read', [
+                                    [[field, '!=', False]]], {'fields': ['name', field]})
+        for record in records:
+            # Before update
+            print("Before...", record[field])
+            
+            if not 'asseco.ng' in record[field]:
+                continue
+
+            # update record login
+            updated_field_value = record[field].replace(
+                'asseco.ng', 'axendit.com')
+
+            # update record login
+            models.execute_kw(db, uid, password, Model, 'write', [
+                record['id'], {field: updated_field_value}])
+            # after update
+            [emp] = models.execute_kw(db, uid, password, Model, 'search_read', [
+                [['id', '=', record['id']]]], {'fields': ['name', field]})
+            
+            print(f"After..., {emp[field]}")
     return True
 
 
 def main():
-    db = config('DB') 
+    db = config('DB')
     username = config('USERNAME')
-    password = config("PASSWORD") 
+    password = config("PASSWORD")
     url = config("HOST")
     models, uid = connector.connect(
         url, db, username, password)
-    return update_login(models, db, uid, password)
+    print("Staring the migration of data...")
+    update_models(models, db, uid, password)
+    print("Updates completed successfully...")
 
 
 if __name__ == "__main__":
